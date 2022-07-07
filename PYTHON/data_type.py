7398c0a5d4286bc61cb65a77bca31d4286192b8a
devabarot2003 <devbarot2003@gmail.com>
2022-07-08 00:03:23 +0530

    BASIC DATA TYPES

---
+++ PYTHON/data_type.py
@@ -0,0 +1,21 @@
+a = 9                                 #Integer
+b = 7.5                               #Float
+c = "Hello, This is Dev."             #String
+
+d = [2,"Dev",True,5.0]                #List
+e = {
+    "Hello":"Hey",
+    "Never":"No",                     #Dictionary
+    "Can I?":"May I?"
+}
+f = (3,5,7,9)                         #Tuple
+
+
+#Use type function to know the type of variables
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
