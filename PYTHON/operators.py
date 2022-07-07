1c09f0d792b8a703770cc86e26f0a3eb7d781341
devabarot2003 <devbarot2003@gmail.com>
2022-07-07 23:49:38 +0530

    TYPES OF OPERATORS

---
+++ PYTHON/operators.py
@@ -0,0 +1,46 @@
+a = 4
+b = 8
+
+#Arithmetic Operators
+print("a + b = ",a+b)
+print("a - b = ",a-b)
+print("a * b = ",a*b)
+print("a / b = ",a/b)
+
+#Assignment Operators
+c = 0
+d = 4
+e = 4
+f = 9
+
+#Short Hand Operators
+c += 3
+d -= 3
+e *= 5
+f /=6
+
+print(c)
+print(d)
+print(e)
+print(f)
+
+#Comparison Operators
+
+a1 = a < b
+a2 = c <= d
+a3 = e > f
+a4 = a >= f
+
+print(a1)
+print(a2)
+print(a3)
+print(a4)
+
+#Logical Operartors
+T = True
+F = False
+
+print ("And testing:", (T and F))
+print ("Or  testing:", (T or F))
+print ("Not testing:", (not F))
+print ("Not testing2:", (not T))
