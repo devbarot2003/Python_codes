36e2917448e41e5a106a85a5ba75ed9734a84c19
devabarot2003 <devbarot2003@gmail.com>
2022-07-17 12:01:54 +0530

    Guess the number using short hand if else.

--- PYTHON/guess.py
+++ PYTHON/guess.py
@@ -13,14 +13,7 @@ def guess_number(user_guess):
         user_guess = int(input("Guess and enter the number from 1 to 20: "))
         guess += 1
         
-        if user_guess not in range(1,21):
-            print("Only enter the number between 1 to 20.\n")
-
-        elif user_guess > comp_guess:
-            print("Too high!!!\n")
-        
-        elif user_guess < comp_guess:
-            print("Too Low!!!\n")
+        print("Only enter the number between 1 to 20.") if user_guess not in range(1,21) else print("Too high!!\n") if user_guess > comp_guess else print("Too Low!!")
 
         
     print("\nRight Guess!!!\n")
